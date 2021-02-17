4b75ced9694e128ff7fda9523ad198eceae56db7
Seth Glenn <Sbgees@yahoo.com>
2021-02-16 21:30:32 -0600

    created put functionality for firebase photos

--- server/config/firebase.js
+++ server/config/firebase.js
@@ -1,6 +1,14 @@
 import firebase from "firebase/app"
 import "firebase/storage"
 
+const storage = firebase.storage().ref();
+
+
+const metaData = {
+    contentType: "image/jpeg"
+};
+const imagesRef = storage.child("images").put(file, metaData);
+
 const firebaseConfig = {
     // For Firebase JS SDK v7.20.0 and later, measurementId is optional
     apiKey: "AIzaSyAX5QK2owUmkaVp-cJA6FBc9MQXoT86xVU",
@@ -11,7 +19,7 @@ const firebaseConfig = {
     appId: "1:149402979838:web:0b7206d6c496588e8f04af",
     measurementId: "G-W4LYEV6WYD"
 };
-
 // init firebase
-
 firebase.initializeApp(firebaseConfig);
+
+
