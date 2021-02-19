af9e62eb34a493af1ae9bf46a30c621dabfd4c75
Seth Glenn <Sbgees@yahoo.com>
2021-02-18 19:58:19 -0600

    added state and component Class for firebase image functions

--- client/src/components/Card/index.js
+++ client/src/components/Card/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import Card from "@material-ui/core/Card";
 import CardHeader from "@material-ui/core/CardHeader";
 import CardMedia from "@material-ui/core/CardMedia";
@@ -6,25 +6,35 @@ import Avatar from "@material-ui/core/Avatar";
 
 import "./style.css";
 
-function RecipeCard() {
-  return (
-    <Card className={classes.root}>
-      <CardMedia
-        className={classes.media}
-        image="" //title of recipe will be pulled into here.
-        title="" //alt text will go here.
-      />
-      <CardHeader
-        avatar={
-          <Avatar> 
-                   
-              
-          </Avatar>
-        }
-        title=" " //title of recipe will be pulled into here.
-        subheader="" //Username could be pulled here.
-      />
-    </Card>
-  );
+class List extends Component {
+
+  state = {
+    imageFood: ""
+  }
+
+
+
+  render() {
+    return (
+      <Card className={classes.root}>
+        <CardMedia
+          className={classes.media}
+          image="" //title of recipe will be pulled into here.
+          title="" //alt text will go here.
+        />
+        <CardHeader
+          avatar={
+            <Avatar>
+
+
+            </Avatar>
+          }
+          title=" " //title of recipe will be pulled into here.
+          subheader="" //Username could be pulled here.
+        />
+      </Card>
+    );
+  }
+
 }
-export default RecipeCard;
+export default List;
