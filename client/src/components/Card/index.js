04b1f6594440a64b57db3003f71c2534791626c5
Seth Glenn <Sbgees@yahoo.com>
2021-02-18 20:36:35 -0600

    created base functions for uploading photos with firebase

--- client/src/components/Card/index.js
+++ client/src/components/Card/index.js
@@ -20,11 +20,31 @@ class List extends Component {
   handleUploadClick = event => {
     event.preventDefault();
     // firebase code to POST/Upload pictures to DB
-    const uploadTask = storageRef.child().put(file)
+    const uploadTask = storageRef.child(imageFood).put(file)
 
-    uploadTast.on("state_changed", 
+    uploadTast.on("state_changed",
       (snapshot) => {
-        
+        const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        console.log('Upload is ' + progress + '% done');
+        switch (snapshot.state) {
+          case firebase.storage.TaskState.PAUSED: // or 'paused'
+            console.log('Upload is paused');
+            break;
+          case firebase.storage.TaskState.RUNNING: // or 'running'
+            console.log('Upload is running');
+            break;
+        }
+      },
+      (error) => {
+        // Handle unsuccessful uploads
+        console.log("Error :>>" + error)
+      },
+      () => {
+        // Handle successful uploads on complete
+        // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+        uploadTask.snapshot.ref.getDownloadURL().then((downloadURL) => {
+          console.log('File available at', downloadURL);
+        });
       }
     )
   }
