fd806a1bde894841777223318b6c9ac4c43f12ed
MidwestMister <70042995+timothykemp@users.noreply.github.com>
2021-02-20 17:01:43 -0600

    updated href to send user to home page if they already have an account

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -139,7 +139,7 @@ function SignUp() {
           </Button>
           <Grid container justify="flex-end">
             <Grid item>
-              <Link href="#" variant="body2">
+              <Link href="/" variant="body2">
                 Already have an account? Sign in
               </Link>
             </Grid>
