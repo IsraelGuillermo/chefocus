b11cb44c60137cb8d629145c46ebe8af5cf9b7c1
Seth Glenn <Sbgees@yahoo.com>
2021-02-20 10:50:29 -0600

    added hreft for signup page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -100,7 +100,7 @@ function Home() {
               </Link>
                         </Grid>
                         <Grid item>
-                            <Link href="#" variant="body2">
+                            <Link href="/signup" variant="body2">
                                 {"Don't have an account? Sign Up"}
                             </Link>
                         </Grid>
