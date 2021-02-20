7d409cc3d6e82f856ac8d170a7b815c98ea55440
MidwestMister <70042995+timothykemp@users.noreply.github.com>
2021-02-19 23:02:03 -0600

    added several fields to Submission page for MVP

--- client/src/pages/Submission.js
+++ client/src/pages/Submission.js
@@ -1,1 +1,100 @@
-import React from "react";
+import React from 'react';
+
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+
+
+const useStyles = makeStyles((theme) => ({
+    paper: {
+        marginTop: theme.spacing(8),
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+    },
+    avatar: {
+        margin: theme.spacing(1),
+        backgroundColor: theme.palette.secondary.main,
+    },
+    form: {
+        width: '100%', // Fix IE 11 issue.
+        marginTop: theme.spacing(1),
+    },
+    submit: {
+        margin: theme.spacing(3, 0, 2),
+    },
+}));
+
+function Submission() {
+    const classes = useStyles();
+
+    return (
+        <Container component="main" maxWidth="xs">
+            <CssBaseline />
+            <div className={classes.paper}>
+                <Typography component="h1" variant="h5">
+                    Submit A Recipe
+                </Typography>
+                <form className={classes.form} noValidate autoComplete='off'>
+                    <TextField
+                        variant="outlined"
+                        margin="normal"
+                        required
+                        fullWidth
+                        id="recipeName"
+                        label="Recipe Name"
+                        name="recipeName"
+                        autoFocus
+                    />
+                    <TextField
+                        variant="outlined"
+                        margin="normal"
+                        required
+                        fullWidth
+                        name="ingredients"
+                        label="Ingredients"
+                        type="text"
+                        id="ingredients"
+                    />
+                    <TextField
+                        variant="outlined"
+                        margin="normal"
+                        required
+                        fullWidth
+                        name="instructions"
+                        label="Instructions"
+                        type="text"
+                        id="instructions"
+                    />
+                    <TextField
+                        variant="outlined"
+                        margin="normal"
+                        required
+                        fullWidth
+                        name="servings"
+                        label="Servings"
+                        type="text"
+                        id="servings"
+                    />
+                    <Button
+                        type="submit"
+                        fullWidth
+                        variant="contained"
+                        color="primary"
+                        className={classes.submit}
+                    >
+                        Create Recipe
+                    </Button>
+                </form>
+            </div>
+        </Container>
+    );
+}
+
+export default Submission;
