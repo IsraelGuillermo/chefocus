3d0d19acca7470c994ca09819c704559257f453e
MidwestMister <70042995+timothykemp@users.noreply.github.com>
2021-02-20 18:53:31 -0600

    removed unnecessary links and checkboxes from login and signup pages

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -79,10 +79,6 @@ function Home() {
             id="password"
             autoComplete="current-password"
           />
-          <FormControlLabel
-            control={<Checkbox value="remember" color="primary" />}
-            label="Remember me"
-          />
           <Button
             type="submit"
             fullWidth
@@ -93,16 +89,6 @@ function Home() {
             Sign In
           </Button>
           <Grid container>
-            <Grid item xs>
-              <Link href="#" variant="body2">
-                Forgot password?
-              </Link>
-            </Grid>
-            <Grid item>
-              <Link href="/signup" variant="body2">
-                {"Don't have an account? Sign Up"}
-              </Link>
-            </Grid>
             <Grid item>
               <Link href="/signup" variant="body2">
                 {"Don't have an account? Sign Up"}
@@ -111,7 +97,7 @@ function Home() {
           </Grid>
         </form>
       </div>
-      <Box mt={8}>
+      <Box mt={5}>
         <Copyright />
       </Box>
     </Container>
