4c2eacdb086a7838ca568a8341b996a827157cc7
Israel Guillermo <israelguillermo22@gmail.com>
2021-02-21 22:59:37 -0600

    updating passport function

--- server/config/passport.js
+++ server/config/passport.js
@@ -1,28 +1,34 @@
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
+const db = require('../models');
 
 // Telling passport we want to use a Local Strategy.
 // In other words, we want login with a username/email and password
+// Telling passport we want to use a Local Strategy. In other words, we want login with a username/email and password
 passport.use(
   new LocalStrategy(
-    // Our user will sign in using an email, rather than a 'username'
+    // Our user will sign in using an email, rather than a "username"
     {
       usernameField: 'email'
     },
-    async (email, password, done) => {
+    (email, password, done) => {
       // When a user tries to sign in this code runs
+      db.User.findOne({
+        where: {
+          email
+        }
+      }).then((dbUser) => {
+        // If there's no user with the given email
+        // If there is a user with the given email, but the password the user gives us is incorrect
+        if (!dbUser || !dbUser.validPassword(password)) {
+          return done(null, false, {
+            message: 'Incorrect email or password.'
+          });
+        }
 
-      const isloginSuccessful = false;
-
-      if (!isloginSuccessful) {
-        // Login failed
-        return done(null, false, {
-          message: 'Invalid user details'
-        });
-      }
-
-      // Login success
-      return done(null, user);
+        // If none of the above, return the user
+        return done(null, dbUser);
+      });
     }
   )
 );
--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -2,8 +2,6 @@ const db = require('../models');
 
 const login = (req, res) => {
   res.json(req.user);
-  console.log(req.body);
-  res.redirect('/explore');
 };
 
 const signup = (req, res) => {
