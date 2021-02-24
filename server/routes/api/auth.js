2233993ec90ae94eb546dd252a3fe5f8c2422c70
Israel Guillermo <israelguillermo22@gmail.com>
2021-02-23 21:53:27 -0600

    adding axios logic for routing

--- server/routes/api/auth.js
+++ server/routes/api/auth.js
@@ -3,10 +3,10 @@ const passport = require('../../config/passport');
 const authController = require('../../controllers/auth');
 const isAuthenticated = require('../../config/middleware/isAuthenticated');
 
-router.use(passport.authenticate('local'));
-
-// Matches with '/api/auth/login'
-router.route('/login', isAuthenticated).post(authController.login);
 router.route('/signup').post(authController.signup);
+
+router.use(passport.authenticate('local'));
+router.route('/login', isAuthenticated).post(authController.login);
+// Matches with '/api/auth/login'
 
 module.exports = router;
