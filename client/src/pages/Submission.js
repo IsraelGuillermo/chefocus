183b23391fc97ab72e924d61654696fc8c283d40
Seth Glenn <Sbgees@yahoo.com>
2021-02-24 21:59:33 -0600

    console log

--- client/src/pages/Submission.js
+++ client/src/pages/Submission.js
@@ -123,7 +123,7 @@ class Form extends Component {
             },
         }).then(res => res.json())
             .then((data) => {
-                console.log(recipe)
+                console.log("Success: ", data)
             }
             ).catch((error) => {
                 console.error('Error:', error);
