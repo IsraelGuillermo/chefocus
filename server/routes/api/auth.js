eaa8b07bef973e6485cfd4c971355acf1754a051
Seth Glenn <Sbgees@yahoo.com>
2021-02-25 22:12:55 -0600

    implemented routing

--- server/routes/api/auth.js
+++ server/routes/api/auth.js
@@ -7,9 +7,8 @@ const isAuthenticated = require('../../config/middleware/isAuthenticated');
 router.route('/signup').post(authController.signup);
 
 router.use(passport.authenticate('local'));
-router.route('/login', isAuthenticated).post(authController.login);
+router.route('/login').post(authController.login);
 // Matches with '/api/auth/login'
 router.route('/signup').post(authController.signup);
-router.route('/recipes').post(recipeController.submit)
 
 module.exports = router;
