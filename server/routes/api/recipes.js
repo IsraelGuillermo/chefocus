47dba73cfc95aee1427f8d89f5127faf01afcc47
Seth Glenn <Sbgees@yahoo.com>
2021-02-26 20:10:12 -0600

    back end data error

--- server/routes/api/recipes.js
+++ server/routes/api/recipes.js
@@ -3,7 +3,7 @@ const router = require('express').Router();
 const recipeController = require("../../controllers/recipes");
 
 
-
 router.route('/').post(recipeController.submit)
+router.route('/').get(recipeController.render)
 
 module.exports = router;
