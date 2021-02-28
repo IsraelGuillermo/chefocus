475a9af59952ff353839fc3d8ac554e0b01883d5
Israel Guillermo <israelguillermo22@gmail.com>
2021-02-28 16:44:19 -0600

    changing recipe cards

--- client/src/components/Card/index.js
+++ client/src/components/Card/index.js
@@ -1,132 +1,52 @@
-import React, { useState, useEffect } from 'react';
-
+import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import clsx from 'clsx';
 import Card from '@material-ui/core/Card';
-import CardHeader from '@material-ui/core/CardHeader';
 import CardMedia from '@material-ui/core/CardMedia';
 import CardContent from '@material-ui/core/CardContent';
 import CardActions from '@material-ui/core/CardActions';
-import Collapse from '@material-ui/core/Collapse';
-import Avatar from '@material-ui/core/Avatar';
-import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
-import { red } from '@material-ui/core/colors';
-import FavoriteIcon from '@material-ui/icons/Favorite';
-import ShareIcon from '@material-ui/icons/Share';
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-import MoreVertIcon from '@material-ui/icons/MoreVert';
-// import { getRecipes } from '../../Utils/API';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import Button from '@material-ui/core/Button';
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles({
   root: {
-    width: 300,
-    backgroundColor: '#99a8b2'
-  },
-  media: {
-    height: 0,
-    paddingTop: '56.25%', // 16:9
-    width: 300
-  },
-  expand: {
-    transform: 'rotate(0deg)',
-    marginLeft: 'auto',
-    transition: theme.transitions.create('transform', {
-      duration: theme.transitions.duration.shortest
-    })
-  },
-  expandOpen: {
-    transform: 'rotate(180deg)'
-  },
-  avatar: {
-    backgroundColor: red[500]
+    maxWidth: 345
   }
-}));
+});
 
 function RecipeReviewCard(props) {
-  const [recipeForm, setRecipeForm] = useState({
-    imageFood: '',
-    recipeName: '',
-    servings: 0,
-    prepHours: 0,
-    prepMinutes: 0,
-    ingredients: '',
-    instructions: ''
-  });
-
-  // const [allResults, setAllResults] = useState([]);
-
   const classes = useStyles();
-  const [expanded, setExpanded] = React.useState(false);
-
-  const handleExpandClick = () => {
-    setExpanded(!expanded);
-  };
-
-  // useEffect(() => {
-  //   getRecipes()
-  //     .then(({ data }) => {
-  //       console.log(data);
-  //       setAllResults(data);
-  //     })
-  //     .catch((error) => {
-  //       console.error('Error:', error);
-  //     });
-  // }, []);
-
-  // getCardData();
 
   return (
     <>
-      {/* {allResults?.map((recipe) => { */}
-      {/* return ( */}
       <Card className={classes.root}>
-        <CardHeader
-          action={
-            <IconButton aria-label="settings">
-              <MoreVertIcon />
-            </IconButton>
-          }
-          title={props.recipeName}
-          subheader="Date of Recipe"
-        />
-        <CardMedia
-          className={classes.media}
-          title="Paella dish"
-          image={`${props.imageFood}`}
-        />
-        {/* <img src={recipe.imageFood} /> */}
-        <CardContent>
-          <Typography
-            variant="body2"
-            color="textSecondary"
-            component="p"
-          ></Typography>
-        </CardContent>
-        <CardActions disableSpacing>
-          <IconButton
-            className={clsx(classes.expand, {
-              [classes.expandOpen]: expanded
-            })}
-            onClick={handleExpandClick}
-            aria-expanded={expanded}
-            aria-label="show more"
-          >
-            <ExpandMoreIcon />
-          </IconButton>
-        </CardActions>
-        <Collapse in={expanded} timeout="auto" unmountOnExit>
+        <CardActionArea>
+          <CardMedia
+            component="img"
+            alt="Contemplative Reptile"
+            height="140"
+            image={props.imageFood}
+            title="Contemplative Reptile"
+          />
           <CardContent>
-            <Typography paragraph>Recipe: </Typography>
-            <Typography paragraph>{/* First bit of instructions */}</Typography>
-            <Typography paragraph>{/* Recipe */}</Typography>
-            <Typography paragraph>{/* Instructions */}</Typography>
-            <Typography>{/* Last Instructions */}</Typography>
+            <Typography gutterBottom variant="h5" component="h2">
+              {props.recipeName}
+            </Typography>
+            <Typography variant="body2" color="textSecondary" component="p">
+              This recipe can be prepared in {props.prepHrs} Hrs and{' '}
+              {props.prepMins} Mins and it provides {props.servings}!
+            </Typography>
           </CardContent>
-        </Collapse>
+        </CardActionArea>
+        <CardActions>
+          <Button size="small" color="primary">
+            Share
+          </Button>
+          <Button size="small" color="primary">
+            Learn More
+          </Button>
+        </CardActions>
       </Card>
-      {/* );
-      })} */}
     </>
   );
 }
