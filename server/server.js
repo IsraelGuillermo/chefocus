5078656075558eec72e7e943e233bd14869b5f43
MidwestMister <70042995+timothykemp@users.noreply.github.com>
2021-03-02 08:57:11 -0600

    fixing heroku errors

--- server/server.js
+++ server/server.js
@@ -33,7 +33,7 @@ app.use(cors(corsOptions));
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('../client/build'));
+  app.use(express.static('client/build'));
 }
 
 // Add routes, API
